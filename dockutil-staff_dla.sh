1bd47cc3f3343ee25db83484d668274d5a736f21
mark-cohen <veberfetzel@gmail.com>
2016-05-24 21:53:49 -0700

    DLA staff dockutil config
    Dockutil base config for DLA staff

---
+++ dockutil-staff_dla.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+DOCKUTIL=/usr/local/bin/dockutil
+
+# remove default apps
+$DOCKUTIL --remove all --no-restart
+
+# add items to dock
+$DOCKUTIL --add '/Applications/System Preferences.app' --position 1 --no-restart 
+$DOCKUTIL --add /Applications/Safari.app --after "System Preferences" --no-restart
+$DOCKUTIL --add /Applications/Firefox.app --after Safari --no-restart
+$DOCKUTIL --add /Applications/Google\ Chrome.app --after Firefox --no-restart
+$DOCKUTIL --add /Applications/Mail.app --after "Google Chrome" --no-restart
+$DOCKUTIL --add /Applications/BusyCal.app --after Mail --no-restart
+$DOCKUTIL --add /Applications/Microsoft\ Office\ 2011/Microsoft\ Word.app/ --after BusyCal --no-restart
+$DOCKUTIL --add /Applications/Microsoft\ Office\ 2011/Microsoft\ Excel.app/ --after "Microsoft Word" --no-restart
+$DOCKUTIL --add '~' --view list --display folder --no-restart
+$DOCKUTIL --add '/Applications' --view list --display folder --position end 
